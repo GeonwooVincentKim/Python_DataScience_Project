bce17ef6813201d5b35082b8f4c00078100c3357
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-04 20:40:24 +0900

    Added and apply abc.Sequence modules.

--- Simple_Python/First.py
+++ Simple_Python/First.py
@@ -2,3 +2,11 @@
     Apply Abstract-Class that includes Data-Structure System.
 """
 import collections.abc as abc
+
+# Check Abstract-Methods that implemented
+# in the Sequence-Class.
+abc.Sequence.__abstractmethods__
+
+# Check Abstract-Methods of MutableSequence Class.
+abc.MutableSequence.__abstractmethods__
+
