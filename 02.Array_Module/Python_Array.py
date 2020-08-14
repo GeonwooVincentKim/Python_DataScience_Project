ee40391e465c0f502be0f741527af1e504835a86
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-14 22:36:00 +0900

    Check the Array Typecode and Byte-Size.

--- 02.Array_Module/Python_Array.py
+++ 02.Array_Module/Python_Array.py
@@ -6,3 +6,18 @@ print(type(a))
 print(a)
 
 print(issubclass(type(a), abc.MutableSequence))
+
+print("\n")
+print(a.typecode)
+print(a.itemsize)
+
+print("\n")
+print(a.buffer_info())
+print(a[0])
+print(a[-1])
+print(len(a))
+
+try:
+    a[5] = 100
+except Exception as e:
+    print(e)
