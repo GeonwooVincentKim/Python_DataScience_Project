08284ca51ff78a4275bc3b9b17a7b53d035ca0b9
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-15 22:22:21 +0900

    Successfully displayed results of module 'pandas' current situation.

--- 03.Pandas_Data_Structure/Apply_Pandas.py
+++ 03.Pandas_Data_Structure/Apply_Pandas.py
@@ -1,3 +1,9 @@
 import pandas as pd
 s = pd.Series([1, 2, 3, 4], index=list('abcd'))
 print(s)
+
+print(s.values)
+print(s.index)
+print("s.shape : {0}, number of dimension of variable 's' : {1}, s.size : {2}".format(
+    s.shape, s.ndim, s.size
+))
