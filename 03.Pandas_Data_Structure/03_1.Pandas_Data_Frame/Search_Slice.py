18cc5038ac6a19664160c7999556f43b04a41617
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-18 21:13:03 +0900

    Import a file as a different way.

--- 03.Pandas_Data_Structure/03_1.Pandas_Data_Frame/Search_Slice.py
+++ 03.Pandas_Data_Structure/03_1.Pandas_Data_Frame/Search_Slice.py
@@ -1,18 +1,18 @@
-from .Generation_DataFrame import *
+from . import Generation_DataFrame as gd
 import numpy as np
 
 
-np.may_share_memory(dfv, df)
-dfv['b'] = 100
-print(dfv)
-print(df)
+np.may_share_memory(gd.dfv, gd.df)
+gd.dfv['b'] = 100
+print(gd.dfv)
+print(gd.df)
 
 # Available to search all of data that includes
 # Column's and Row's data.
-print(df.iloc[0])
+print(gd.df.iloc[0])
 
 # Set One-Row and One-Columns.
-print(df.iloc[0, 0])
+print(gd.df.iloc[0, 0])
 
-s = df.iloc[:, 0]
+s = gd.df.iloc[:, 0]
 print(s)
