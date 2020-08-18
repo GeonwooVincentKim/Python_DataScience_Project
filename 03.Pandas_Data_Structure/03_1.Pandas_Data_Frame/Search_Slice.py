b8dbcdb833862bb8d9104a7d4a38ded4656cc54b
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-18 20:21:10 +0900

    Searching data from df that can search part of data or all of data by using 'list'.

--- 03.Pandas_Data_Structure/03_1.Pandas_Data_Frame/Search_Slice.py
+++ 03.Pandas_Data_Structure/03_1.Pandas_Data_Frame/Search_Slice.py
@@ -5,3 +5,10 @@ import numpy as np
 np.may_share_memory(dfv, df)
 dfv['b'] = 100
 print(dfv)
+print(df)
+
+# Available to search all of data that includes
+# Column's and Row's data.
+print(df.iloc[0])
+print(df.iloc[0, 0])
+
